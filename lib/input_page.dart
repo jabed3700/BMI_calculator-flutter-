41fe71ec3b8322fec66a3ad379eed24a59a055f9
tamim08 <tamim008bd@gmail.com>
2021-06-15 18:24:47 +0600

    final vs const keyword

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'input_page.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -21,12 +25,12 @@ class _InputPageState extends State<InputPage> {
                 children: [
                   Expanded(
                     child: ReusableCard(
-                      colour: Color(0xFF1D1E33),
+                      colour: activeCardColor,
                     ),
                   ),
                   Expanded(
                     child: ReusableCard(
-                      colour: Color(0xFF1D1E33),
+                      colour: activeCardColor,
                     ),
                   )
                 ],
@@ -34,7 +38,7 @@ class _InputPageState extends State<InputPage> {
             ),
             Expanded(
               child: ReusableCard(
-                colour: Color(0xFF1D1E33),
+                colour: activeCardColor,
               ),
             ),
             Expanded(
@@ -42,17 +46,23 @@ class _InputPageState extends State<InputPage> {
                 children: [
                   Expanded(
                     child: ReusableCard(
-                      colour: Color(0xFF1D1E33),
+                      colour: activeCardColor,
                     ),
                   ),
                   Expanded(
                     child: ReusableCard(
-                      colour: Color(0xFF1D1E33),
+                      colour: activeCardColor,
                     ),
                   ),
                 ],
               ),
             ),
+            Container(
+              width: double.infinity,
+              height: bottomContainerHeight,
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+            )
           ],
         ));
   }
