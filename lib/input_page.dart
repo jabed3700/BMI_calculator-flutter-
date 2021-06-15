2d8af00124abda002c552f41a56f039e1525a9e5
tamim08 <tamim008bd@gmail.com>
2021-06-15 16:10:24 +0600

    Refactor flutter widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,7 @@
-
 import 'package:flutter/material.dart';
+
 import 'input_page.dart';
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -10,15 +11,75 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      colour: Color(0xFF1D1E33),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: Color(0xFF1D1E33),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(
+                colour: Color(0xFF1D1E33),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      colour: Color(0xFF1D1E33),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      colour: Color(0xFF1D1E33),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: colour,
       ),
     );
   }
 }
+
+// Container(
+// margin: EdgeInsets.all(15.0),
+// width: 170.0,
+// height: 200.0,
+// decoration: BoxDecoration(
+// borderRadius: BorderRadius.circular(10.0),
+// color:Color(0xFF1D1E33),
+// ),
+// ),
